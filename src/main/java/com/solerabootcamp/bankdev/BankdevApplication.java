0e7b705f50c95862249977c3cf2117786e74e85d
Pintado <alberto.pintado@solera.com>
2022-11-23 09:56:46 +0100

    first commit

---
+++ src/main/java/com/solerabootcamp/bankdev/BankdevApplication.java
@@ -0,0 +1,13 @@
+package com.solerabootcamp.bankdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solerabootcamp/bankdev/BankdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solerabootcamp.bankdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
