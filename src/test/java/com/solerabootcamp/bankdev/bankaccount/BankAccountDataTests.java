20ab6f7e085ab2ea8a0502a7bcad8ffdb494a8d3
Pintado <alberto.pintado@solera.com>
2022-11-23 14:26:36 +0100

    BankAccountDataTests get test

---
+++ src/test/java/com/solerabootcamp/bankdev/bankaccount/BankAccountDataTests.java
@@ -0,0 +1,43 @@
+package com.solerabootcamp.bankdev.bankaccount;
+
+import com.solerabootcamp.bankdev.user.Data;
+import com.solerabootcamp.bankdev.user.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class BankAccountDataTests {
+
+    private final BankAccountsData bankAccountsData = new BankAccountsData();
+
+    @Test
+    public void Data_WhenGetAfterCreation_ReturnsBankAccountsAndThirdIsExpected() {
+        List<BankAccount> bankAccounts = this.bankAccountsData.getUsers();
+        int usersCount = this.bankAccountsData.getCount();
+
+        BankAccount thirdBankAccount = bankAccounts.stream().filter(streamUser -> streamUser.getId() == 3).findFirst().get();
+
+        assertEquals(9, usersCount);
+        assertEquals(3, thirdBankAccount.getId());
+        assertEquals("Prueba 3", thirdBankAccount.getName());
+    }
+
+    @Test
+    public void Data_WhenGetAfterCreation_ReturnsBankAccountsAndThirdIsExpected() {
+        List<BankAccount> bankAccounts = this.bankAccountsData.getUsers();
+        int usersCount = this.bankAccountsData.getCount();
+
+        BankAccount thirdBankAccount = bankAccounts.stream().filter(streamUser -> streamUser.getId() == 3).findFirst().get();
+
+        assertEquals(9, usersCount);
+        assertEquals(3, thirdBankAccount.getId());
+        assertEquals("Prueba 3", thirdBankAccount.getName());
+    }
+}
